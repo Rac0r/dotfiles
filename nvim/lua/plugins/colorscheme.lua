7de929fb937b92826032831ab70e50205a279fe6
Jonas <44616049+Rac0r@users.noreply.github.com>
2026-06-20 08:48:04 +0200

    switched to monokai-pro

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,65 @@
 return {
   {
-    "catppuccin/nvim",
+    "loctvl842/monokai-pro.nvim",
     lazy = false,
-    name = "catppuccinuccin",
     priority = 1000,
     config = function()
-      require("catppuccin").setup()
-      vim.cmd.colorscheme("catppuccin-mocha")
+      require("monokai-pro").setup({
+        transparent_background = false,
+        terminal_colors = true,
+        devicons = true,
+        styles = {
+          comment = { italic = true },
+          keyword = { italic = true },
+          type = { italic = true },
+          storageclass = { italic = true },
+          structure = { italic = true },
+          parameter = { italic = true },
+          annotation = { italic = true },
+          tag_attribute = { italic = true },
+        },
+        filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+        day_night = {
+          enable = true,
+          day_filter = "pro",
+          night_filter = "spectrum",
+        },
+        inc_search = "background", -- underline | background
+        background_clear = {
+          "toggleterm",
+          "telescope",
+          "renamer",
+          "notify",
+        },
+        plugins = {
+          bufferline = {
+            underline_selected = true,
+            underline_visible = true,
+            underline_fill = true,
+            bold = true,
+          },
+          indent_blankline = {
+            context_highlight = "pro", -- default | pro
+            context_start_underline = false,
+          },
+        },
+        override = function(scheme)
+          return {}
+        end,
+        override_palette = function(filter)
+          return {}
+        end,
+        override_scheme = function(scheme, palette, colors)
+          return {}
+        end,
+      })
     end,
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "monokai-pro",
     },
   },
 }
